f0eb94430f165e1978a3aa428a715637c8160534
pakut2 <kutny.pawel@gmail.com>
2021-09-25 23:17:40 +0200

    deploy

--- server/apps/shared/database/database.module.ts
+++ server/apps/shared/database/database.module.ts
@@ -19,8 +19,11 @@ import { User } from "../../server/src/users/entities/user.entity";
         database: configService.get("POSTGRES_DB"),
         entities: [PublicFile, Post, User],
         synchronize: true,
+        ssl: true,
         extra: {
-          ssl: true,
+          ssl: {
+            rejectUnauthorized: false,
+          },
         },
       }),
     }),
